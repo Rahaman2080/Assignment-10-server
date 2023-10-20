97f50e04e2d3a56e97fa93067c7a1647567bb5c9
Rahaman2080 <rahaman2080@gmail.com>
2023-10-21 02:22:59 +0600

    added app.post for added product from client site

--- index.js
+++ index.js
@@ -25,6 +25,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productCollection = client.db("ProductDB").collection('prducts');
+
+    app.post('/products', async(req,res)=>{
+        const newProduct = req.body;
+        console.log(newProduct);
+        const result = await productCollection.insertOne(newProduct);
+        res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
